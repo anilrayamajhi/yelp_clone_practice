0745b79f96653fa32f800064f055f488621df750
Anil Rayamajhi <anilrm07@gmail.com>
2016-11-08 20:44:57 -0800

    css implementaton: precss autoprefixer cssnano

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import styles from './styles.module.css'
+
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+
 const App = React.createClass({
   render: function() {
-    return (<div>Text text text</div>);
+    return (
+      <div className={styles['container']}>
+        Ganey Bahadur
+      </div>
+    );
   }
 });
+
+const mountNode = document.querySelector('#root');
+ReactDOM.render(<App />, mountNode);
--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,20 @@ const
   modules = join(root, 'node_modules'),
   dest    = join(root, 'dist'),
 
+  NODE_ENV = process.env.NODE_ENV,
   isDev = NODE_ENV === 'development'
 
 var config = getConfig({
-  isDev = isDev,
+  isDev: isDev,
   in: join(src, 'app.js'),
   out: dest,
   clearBeforeBuild: true
 })
+
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+])
 
 module.exports = config
